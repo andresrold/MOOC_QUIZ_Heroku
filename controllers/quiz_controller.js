1ebde187144d1578fda459a88c1632337f060e68
Andres Roldan <andresrold@gmail.com>
2015-08-02 19:09:53 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -43,3 +43,22 @@ exports.answer = function(req, res) {
     	res.render('quizes/answer', { quiz: req.quiz, respuesta: 'Incorrecto'});
     }
 };
+
+// GET /quizes/new
+exports.new = function(req, res) {
+    var quiz = models.Quiz.build( // crea un objeto Quiz
+        { pregunta: "Pregunta", respuesta: "Respuesta" }
+    );
+
+    res.render('quizes/new', {quiz: quiz});
+};
+// POST /quizes/create
+exports.create = function(req, res) {
+    var quiz = models.Quiz.build( req.body.quiz );
+    quiz.save(
+        {fields: ["pregunta", "respuesta"]
+    }).then(function() {
+        res.redirect('/quizes');
+    });
+};
+
